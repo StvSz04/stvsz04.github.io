75eab05e4e330e510e6a179ad31f0c17a32a55ba
Steven <iamstevensanchez04@gmail.com>
2026-07-04 20:19:25 -0500

    Website is up to date

---
+++ sidebar.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebarContainer = document.getElementById("sidebar-container");
+    
+    if (sidebarContainer) {
+        sidebarContainer.innerHTML = `
+            <div class="side-bar">
+                <h3>General</h3>
+                <ul>
+                    <li><a href="index.html">Home</a></li> 
+                    <li><a href="aboutme.html">About Me</a></li>    
+                    <li><a href="aboutmentor.html">About Mentor</a></li>
+                    <li><a href="aboutproject.html">About Project</a></li>
+                </ul>
+
+                <h3>Weekly log</h3>
+                <ul>
+                    <li><a href="week-1.html">Week 1</a></li>
+                    <li><a href="week-2.html">Week 2</a></li>
+                    <li><a href="week-3.html">Week 3</a></li>
+                    <li><a href="week-4.html">Week 4</a></li>
+                </ul>
+            </div>
+        `;
+    }
+});
