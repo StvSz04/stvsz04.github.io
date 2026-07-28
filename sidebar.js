1626376ce72ed1a8b39d65bcd059fba002547bb8
Steven <iamstevensanchez04@gmail.com>
2026-07-28 13:17:27 -0500

    Week 8 and week 7 edit

--- sidebar.js
+++ sidebar.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     <li><a href="week-5.html">Week 5</a></li>
                     <li><a href="week-6.html">Week 6</a></li>
                     <li><a href="week-7.html">Week 7</a></li>
+                    <li><a href="week-8.html">Week 8</a></li>
                 </ul>
             </div>
         `;
